75e9d5e616bccf321042fe76a891f1c51e0c3873
Juliano <juliano.moreira.aleixo@gmail.com>
2026-05-15 00:13:25 -0300

    :sparkles: Feat - Adding initial layout for the tailwind example

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,5 +1,9 @@
 const Header = () => {
-    return <div>header</div>;
+    return (
+        <div className="h-16 w-full bg-indigo-500 text-white font-medium text-2xl flex items-center justify-center fixed">
+            Painel de Administração
+        </div>
+    );
 };
 
 export default Header;
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <section className="pt-18 mx-8 md:mx-12 min-h-screen flex flex-col">
+            <h1 className="text-4xl my-4">Bem vindo, Administrador!</h1>
+        </section>
+    );
 };
 
 export default Home;
