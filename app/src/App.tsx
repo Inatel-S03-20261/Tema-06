5477283feed91e4e5bdf3f95b43b96fd6ae3da1e
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2026-05-21 19:50:54 -0300

    :bug: Fix - Adding Header on PageLayout to avoid using it on App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import PlayerDetails from "./pages/PlayerDetails";
 import Players from "./pages/Players";
@@ -8,11 +7,13 @@ import TradeDashboard from "./pages/Trades";
 const App = () => {
     return (
         <>
-            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/jogadores" element={<Players />} />
-                <Route path="/jogadores/:playerId" element={<PlayerDetails />} />
+                <Route
+                    path="/jogadores/:playerId"
+                    element={<PlayerDetails />}
+                />
                 <Route path="/trocas" element={<TradeDashboard />} />
             </Routes>
         </>
--- app/src/components/PageLayout.tsx
+++ app/src/components/PageLayout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Header from "./Header";
 
 type PageLayoutProps = {
     title: string;
@@ -8,11 +9,16 @@ type PageLayoutProps = {
 
 const PageLayout = ({ title, children, className = "" }: PageLayoutProps) => {
     return (
-        <section className={`pt-24 mx-8 md:mx-12 min-h-screen ${className}`.trim()}>
-            <h1 className="text-4xl mb-6">{title}</h1>
+        <>
+            <Header />
+            <section
+                className={`pt-24 mx-8 md:mx-12 min-h-screen ${className}`.trim()}
+            >
+                <h1 className="text-4xl mb-6">{title}</h1>
 
-            {children}
-        </section>
+                {children}
+            </section>
+        </>
     );
 };
 
